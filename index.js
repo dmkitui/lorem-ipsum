f8aecf92e93f7ba3bb402604720a52520204a6e7
Daniel Masake Kitui <daniel.kitui@andela.com>
2018-10-05 08:06:40 +0300

    Edit variable name

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let number;
 
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  let randomStuff = [];
+  let randomOutput = [];
   if (request.method === "POST") {
     let values = {};
     request.on("data", function(data) {
@@ -19,13 +19,13 @@ http.createServer(function (request, response) {
         }
         selector = values.selector;
         number = values.number;
-        randomStuff = loremGenerator[selector](number);
-        renderer.view("home", randomStuff, response);
+        randomOutput = loremGenerator[selector](number);
+        renderer.view("home", randomOutput, response);
         response.end();
     })
   } else {
     renderer.view("home", [], response);
     response.end();
   }
-}).listen(1337, "127.0.0.1");
-console.log("Running!");
+}).listen(3000, "127.0.0.1");
+console.log("Lorem Ipsum generator running at http://localhost:3000/");
